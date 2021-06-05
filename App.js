742eef25092b5ea6166e4f219e0bab3e5962d696
vinay <vinaykvn29@gmail.com>
2021-06-05 13:10:45 +0530

    dev merge

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text,Image , Button} from 'react-native';
+import constants from 'expo-constants';
+import { useDimensions } from '@react-native-community/hooks';
+import WelcomeScreen from './app/screen/WelcomeScreen';
+import ViewImageScreen from './app/screen/ViewImageScreen';
+import Profile from './app/screen/Profile';
+ 
+export default function App() 
+{
 
-export default function App() {
-
-
-  console.log("App executed");
-  //  let x;
-
-  // x.toString();
-
-
+  
+  console.log(require("./app/assets/splash.png"));
+  console.log(useDimensions());
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app12345! Dev1</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+     <Profile/>
+
   );
 }
 
@@ -23,7 +24,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:"column",
+    marginTop:constants.statusBarHeight,
+  },
+  stretch: {
+    width: 200,
+    height: 200,
+    marginTop : 100,
+    resizeMode: 'stretch',
+    transform: [{ rotate: '45deg' }]
+    
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
   },
 });
